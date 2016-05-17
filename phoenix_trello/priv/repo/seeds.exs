c6e7534ee98725ab226ca6fb9e030724f8556c8c
agarciafdz <agarciafdz@gmail.com>
2016-05-17 08:48:17 -0400

    the tutorial up to day 5

--- phoenix_trello/priv/repo/seeds.exs
+++ phoenix_trello/priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhoenixTrello.{Repo, User}
+[
+  %{first_name: "John",
+    last_name: "Doe",
+    email: "john@phoenixtrello.com",
+    password: "123345678"
+   },
+]
+|> Enum.map(&User.changeset(%User{}, &1))
+|> Enum.each(&Repo.insert!(&1))
--- phoenix_trello/web/router.ex
+++ phoenix_trello/web/router.ex
@@ -11,13 +11,21 @@ defmodule PhoenixTrello.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/api" do
     pipe_through :api
 
     scope "/v1" do
-      post "/registrations", RegistrationControllel, :create
+      post "/registrations", RegistrationController, :create
+
+      post "/sessions", SessionController, :create
+      delete "/sessions", SessionController, :delete
+
+      get "/current_user", CurrentUserController, :show
     end
   end
 
